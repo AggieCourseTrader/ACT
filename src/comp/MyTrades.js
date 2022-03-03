bbbd79f92d531669098daeea0ecb83f0f378e124
Sean Wangnick <swangnick@gmail.com>
2022-03-03 12:33:08 -0600

    Update MyTrades.js

--- src/comp/MyTrades.js
+++ src/comp/MyTrades.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 // import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import {onAuthStateChanged, auth} from '../firebase-config'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 // import Toolbar from '@mui/material/Toolbar';
@@ -13,76 +13,80 @@ import Paper from '@mui/material/Paper';
 // import Chart from './Chart';
 import MyListings from './MyListings';
 import MyMatches from './MyMatches';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const mdTheme = createTheme();
 
 function MyTrades() {
-  
+
   // const [count, setCount] = useState(0);
-  
+
   let navigate = useNavigate();
-  
+
   useEffect(() => {
-   onAuthStateChanged(auth, (user) => {
-     if (user) {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
         // const uid = user.uid;
-     } else {
-       navigate("/")
-     }
+      } else {
+        navigate("/")
+      }
     });
- 
-   }, /*removed dependency array*/)
-  
+
+  }, /*removed dependency array*/)
+
   return (
       <div>
+        <Navbar/>
         <ThemeProvider theme={mdTheme}>
-        <Box sx={{ display: 'flex' }}>
-          <CssBaseline />
-          <Box
-              component="main"
-              sx={{
-                backgroundColor: (theme) =>
-                    theme.palette.mode === 'light'
-                        ? theme.palette.grey[100]
-                        : theme.palette.grey[900],
-                flexGrow: 1,
-                height: '80vh',
-                overflow: 'auto',
-              }}
-          >
-            <Container maxWidth="lg" sx={{ mt: 5, mb: 5 }}>
-              <Grid container spacing={3}>
-                {/* My Listings */}
-                <Grid item xs={12} sm={6} md={6} lg={6}>
-                  <Paper
-                      sx={{
-                        p: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        height: 400,
-                      }}
-                  >
-                    <MyListings />
-                  </Paper>
+          <Box sx={{display: 'flex'}}>
+            <CssBaseline/>
+            <Box
+                component="main"
+                sx={{
+                  backgroundColor: (theme) =>
+                      theme.palette.mode === 'light'
+                          ? theme.palette.grey[100]
+                          : theme.palette.grey[900],
+                  flexGrow: 1,
+                  height: '80vh',
+                  overflow: 'auto',
+                }}
+            >
+              <Container maxWidth="lg" sx={{mt: 5, mb: 5}}>
+                <Grid container spacing={3}>
+                  {/* My Listings */}
+                  <Grid item xs={12} sm={6} md={6} lg={6}>
+                    <Paper
+                        sx={{
+                          p: 2,
+                          display: 'flex',
+                          flexDirection: 'column',
+                          height: 400,
+                        }}
+                    >
+                      <MyListings/>
+                    </Paper>
+                  </Grid>
+                  {/* My Matches */}
+                  <Grid item xs={12} sm={6} md={6} lg={6}>
+                    <Paper
+                        sx={{
+                          p: 2,
+                          display: 'flex',
+                          flexDirection: 'column',
+                          height: 400,
+                        }}
+                    >
+                      <MyMatches/>
+                    </Paper>
+                  </Grid>
                 </Grid>
-                {/* My Matches */}
-                <Grid item xs={12} sm={6} md={6} lg={6}>
-                  <Paper
-                      sx={{
-                        p: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        height: 400,
-                      }}
-                  >
-                    <MyMatches />
-                  </Paper>
-                </Grid>
-              </Grid>
-            </Container>
+              </Container>
+            </Box>
           </Box>
-        </Box>
-      </ThemeProvider>
+        </ThemeProvider>
+        <Footer/>
       </div>
   );
 }

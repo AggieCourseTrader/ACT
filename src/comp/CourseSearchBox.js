a5b007d6f2740684db31f52c971f194d0255208a
AmritSd <amritsmail2002@gmail.com>
2022-03-06 15:59:30 -0600

    abstracted aync out of useEffect

--- src/comp/CourseSearchBox.js
+++ src/comp/CourseSearchBox.js
@@ -30,43 +30,80 @@ function CourseSearchBox({ db, selectionCallBack }) {
 
 	console.log(sectionSelected);
 	//* Updates search results whenever something is typed
-	useEffect(async () => {
-		let text = searchText;
-		let courseText = text.replace(/\s*\d+\s*/g, '').replace(/\s*/g, '');
-		let courseNumber = text.replace(/\D*/g, '');
-		let arr = [];
-		console.log(courseText);
-		if(queryCounter > 20 || text.length < 2) {
-			setSearchResults([]);
-			return;
-		}
+	useEffect(() => {
 
-		let q = false;
-		// * If courseText is greaterthan >= 2
-		// Search course first, then filter using number
-		if(courseText.length >= 2) {
-			q = query(collection(db, "majors"), where("keywords", 'array-contains', courseText.toUpperCase()));
-			const querySnapshot = await getDocs(q);
-
-			querySnapshot.forEach((doc) => {
-				let major = doc.data().name;
-				let courseMap = doc.data().course_map;
-				// Match the numbers in a complicated way
-				if(courseNumber.length >= 1) {
-					doc.data().courses.forEach((c) => {
-						c = c.toString();
-						let match = true;
-						for(let i = 0; i < courseNumber.length; i++) {
-							if(i >= c.length) {
-								match = false;
-								break;
+		const f = async () => {
+			let text = searchText;
+			let courseText = text.replace(/\s*\d+\s*/g, '').replace(/\s*/g, '');
+			let courseNumber = text.replace(/\D*/g, '');
+			let arr = [];
+			console.log(courseText);
+			if(queryCounter > 20 || text.length < 2) {
+				setSearchResults([]);
+				return;
+			}
+	
+			let q = false;
+			// * If courseText is greaterthan >= 2
+			// Search course first, then filter using number
+			if(courseText.length >= 2) {
+				q = query(collection(db, "majors"), where("keywords", 'array-contains', courseText.toUpperCase()));
+				const querySnapshot = await getDocs(q);
+	
+				querySnapshot.forEach((doc) => {
+					let major = doc.data().name;
+					let courseMap = doc.data().course_map;
+					// Match the numbers in a complicated way
+					if(courseNumber.length >= 1) {
+						doc.data().courses.forEach((c) => {
+							c = c.toString();
+							let match = true;
+							for(let i = 0; i < courseNumber.length; i++) {
+								if(i >= c.length) {
+									match = false;
+									break;
+								}
+								if(c[i] !== courseNumber[i]) {
+									match = false;
+									break
+								}
 							}
-							if(c[i] !== courseNumber[i]) {
-								match = false;
-								break
+							if(match) {
+								let name = major + " "  + c;
+								let crns = courseMap[parseInt(c)];
+								arr.push({
+									name : name,
+									crns : crns
+								});
 							}
-						}
-						if(match) {
+						});
+					}
+					// No numbers were given add everything
+					else {
+						doc.data().courses.forEach((c) => {
+							let name = major + " "  + c;
+							let crns = courseMap[parseInt(c)];
+							arr.push({
+								name : name,
+								crns : crns
+							});
+						});
+					}
+					console.log(doc.id, " => ", doc.data());
+				});
+			}
+			// * Else filter using number if courseNumber is greaterthan > 2 (limit 3)
+			// Filter using coursename from keywords
+			else if(courseNumber.length > 2) {
+				q = query(collection(db, "majors"), where("courses", 'array-contains', courseNumber));
+				const querySnapshot = await getDocs(q);
+				querySnapshot.forEach((doc) => {
+					let major = doc.data().name;
+					let courseMap = doc.data().course_map;
+					let c = courseNumber;
+					// Match the major
+					if(courseText.length >= 1) {
+						if(doc.data().keywords.includes(courseText.toUpperCase())) {
 							let name = major + " "  + c;
 							let crns = courseMap[parseInt(c)];
 							arr.push({
@@ -74,34 +111,9 @@ function CourseSearchBox({ db, selectionCallBack }) {
 								crns : crns
 							});
 						}
-					});
-				}
-				// No numbers were given add everything
-				else {
-					doc.data().courses.forEach((c) => {
-						let name = major + " "  + c;
-						let crns = courseMap[parseInt(c)];
-						arr.push({
-							name : name,
-							crns : crns
-						});
-					});
-				}
-				console.log(doc.id, " => ", doc.data());
-			});
-		}
-		// * Else filter using number if courseNumber is greaterthan > 2 (limit 3)
-		// Filter using coursename from keywords
-		else if(courseNumber.length > 2) {
-			q = query(collection(db, "majors"), where("courses", 'array-contains', courseNumber));
-			const querySnapshot = await getDocs(q);
-			querySnapshot.forEach((doc) => {
-				let major = doc.data().name;
-				let courseMap = doc.data().course_map;
-				let c = courseNumber;
-				// Match the major
-				if(courseText.length >= 1) {
-					if(doc.data().keywords.includes(courseText.toUpperCase())) {
+					}
+					// No major was specified
+					else {
 						let name = major + " "  + c;
 						let crns = courseMap[parseInt(c)];
 						arr.push({
@@ -109,90 +121,87 @@ function CourseSearchBox({ db, selectionCallBack }) {
 							crns : crns
 						});
 					}
-				}
-				// No major was specified
-				else {
-					let name = major + " "  + c;
-					let crns = courseMap[parseInt(c)];
-					arr.push({
-						name : name,
-						crns : crns
-					});
-				}
-				console.log(doc.id, " => ", doc.data());
-			});
-		}
-		setSearchResults(arr);
-		setQueryCounter(queryCounter + 1);
+					console.log(doc.id, " => ", doc.data());
+				});
+			}
+			setSearchResults(arr);
+			setQueryCounter(queryCounter + 1);
+		};
+
+		f();
 	}, [searchText]);
 
 	//* Runs whenever a course is selected from the dropdown
-	useEffect(async () => {
-		setSectionSelected('');
-		setSectionResults([]);
+	useEffect(() => {
 
-		if(courseSelected === undefined) {
-			return;
-		}
-		let arr = [];
-
-		let q = await getCoursesByName(courseSelected.name);
-
-
-		q.forEach((section) => {
-			let lecDays = []
-			let lecTimes = ''
-			let labDays = []
-			let labTimes = ''
-			console.log(section.data());
-			
-			Object.entries(section.data().meeting_times).forEach((t) => {
-				let day = t[0];
-				let times = t[1];
-				times.forEach((t) => {
-					if (t.substring(t.length - 3) === "LAB") {
-						labDays.push(day);
-						labTimes = t.substring(0, t.length - 4);
-					} else if (t.substring(t.length - 3) === "LEC") {
-						lecDays.push(day);
-						lecTimes = t.substring(0, t.length - 4);
-					} 
-					
+		const f = async () => {
+			setSectionSelected('');
+			setSectionResults([]);
+	
+			if(courseSelected === undefined) {
+				return;
+			}
+			let arr = [];
+	
+			let q = await getCoursesByName(courseSelected.name);
+	
+	
+			q.forEach((section) => {
+				let lecDays = []
+				let lecTimes = ''
+				let labDays = []
+				let labTimes = ''
+				console.log(section.data());
+				
+				Object.entries(section.data().meeting_times).forEach((t) => {
+					let day = t[0];
+					let times = t[1];
+					times.forEach((t) => {
+						if (t.substring(t.length - 3) === "LAB") {
+							labDays.push(day);
+							labTimes = t.substring(0, t.length - 4);
+						} else if (t.substring(t.length - 3) === "LEC") {
+							lecDays.push(day);
+							lecTimes = t.substring(0, t.length - 4);
+						} 
+						
+					});
 				});
+				
+				labDays.sort((x, y) => {
+					let p = ['M', 'T', 'W', 'R', 'F', 'S']
+					if(p.indexOf(x) < p.indexOf(y)) {
+						return -1;
+					}
+					else if(p.indexOf(x) > p.indexOf(y)) {
+						return 1;
+					}
+				});
+	
+				lecDays.sort((x, y) => {
+					let p = ['M', 'T', 'W', 'R', 'F', 'S']
+					if(p.indexOf(x) < p.indexOf(y)) {
+						return -1;
+					}
+					else if(p.indexOf(x) > p.indexOf(y)) {
+						return 1;
+					}
+				});
+	
+	
+				let item = {
+					'section' : section.data().section,
+					'lec' : lecDays.join('') + " " + lecTimes + " LEC",
+					'lab' : (labDays === []) ? '' : labDays.join('') + " " + labTimes + " LAB"
+				};
+				console.log(item);
+				arr.push(item);
 			});
-			
-			labDays.sort((x, y) => {
-				let p = ['M', 'T', 'W', 'R', 'F', 'S']
-				if(p.indexOf(x) < p.indexOf(y)) {
-					return -1;
-				}
-				else if(p.indexOf(x) > p.indexOf(y)) {
-					return 1;
-				}
-			});
+	
+			setSectionResults(arr);
+		};
 
-			lecDays.sort((x, y) => {
-				let p = ['M', 'T', 'W', 'R', 'F', 'S']
-				if(p.indexOf(x) < p.indexOf(y)) {
-					return -1;
-				}
-				else if(p.indexOf(x) > p.indexOf(y)) {
-					return 1;
-				}
-			});
-
-
-			let item = {
-				'section' : section.data().section,
-				'lec' : lecDays.join('') + " " + lecTimes + " LEC",
-				'lab' : (labDays === []) ? '' : labDays.join('') + " " + labTimes + " LAB"
-			};
-			console.log(item);
-			arr.push(item);
-		});
-
-		setSectionResults(arr);
-		
+		f();
 	}, [courseSelected]);
 
 	// let getCourseList = async (text) => {
@@ -222,7 +231,7 @@ function CourseSearchBox({ db, selectionCallBack }) {
 
 				<Autocomplete
 				disabled={(courseSelected === undefined) ? true : false}
-				autoHighlight
+					autoHighlight
 				onChange={(e, v) => {console.log(v)}}
 				
 				sx = {{width : 300}}

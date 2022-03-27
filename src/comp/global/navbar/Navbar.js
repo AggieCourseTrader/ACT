b19ee90dcf094f8d81e248d3d0b2f2085461c380
Jack Cleary <jack.cleary@tamu.edu>
2022-03-27 16:24:56 -0500

    deleting unused variables

--- src/comp/global/navbar/Navbar.js
+++ src/comp/global/navbar/Navbar.js
@@ -8,16 +8,11 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
-import Tooltip from '@mui/material/Tooltip';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { Link } from "react-router-dom";
 import { signOut } from "../../../firebase-config";
 import { useNavigate } from "react-router-dom";
-
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar(props) {
   const [anchorElNav, setAnchorElNav] = React.useState(null);

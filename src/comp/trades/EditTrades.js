ba021bc88a0e9a8b2646df649bd7bff1568f8011
ryanjw0903 <60299798+ryanjw0903@users.noreply.github.com>
2022-04-03 16:44:29 -0500

    unused var fix

--- src/comp/trades/EditTrades.js
+++ src/comp/trades/EditTrades.js
@@ -1,5 +1,4 @@
 import React, { useState} from 'react';
-import { useNavigate } from 'react-router-dom'
 import CourseSearchBox  from '../global/courseSearchBox/CourseSearchBox'
 import {createTrade, deleteTrade, db, updateTrade} from '../global/dbFunctions/CrudFunctions';
 import { makeStyles } from '@mui/styles';

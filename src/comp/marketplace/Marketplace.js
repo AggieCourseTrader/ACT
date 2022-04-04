e9a1145660adea4f0276550a4d0956e5d423b02e
Jack Cleary <jack.cleary@tamu.edu>
2022-04-04 09:11:49 -0500

    updated alerts on marketplace page

--- src/comp/marketplace/Marketplace.js
+++ src/comp/marketplace/Marketplace.js
@@ -11,6 +11,8 @@ import Navbar from '../global/navbar/Navbar';
 import Footer from "../global/Footer";
 import { updateTradeMatch, createTrade } from "../global/dbFunctions/CrudFunctions"
 import Chip from '@mui/material/Chip';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
@@ -29,6 +31,8 @@ function Marketplace() {
 
   const [myTradeRows, setMyTradeRows] = useState([]);
   const myTrades = useRef(null);
+
+  const [alert, setAlert] = useState(null)
 
   useEffect(() => {
    onAuthStateChanged(auth, (user) => {
@@ -309,15 +313,35 @@ function Marketplace() {
             </Box>
             <Box sx = {{textAlign: "center", m: 2}}>
               <Button variant = "outlined" justifyContent = "center"
-                onClick={() => {
+                onClick={ async () => {
                   if (addClass.class !== '' && addClass.section !== '' && dropClass.class !== '' && dropClass.section !== '') {
-                    createTrade(user.uid, dropClass.crn, addClass.crn);
-                    alert('trade request created \nadd ' + addClass.class + ': ' + addClass.section + ' and drop ' + dropClass.class + ': ' + dropClass.section);
+                    const val = await createTrade(user.uid, dropClass.crn, addClass.crn);
+                    console.log('val: ' + val);
+                    if(val !== null) {
+                      // this doesnt show up. look into mui alerts to figure how this works
+                      console.log("here"); // this line is executed, but below lines don't do anything
+                      setAlert(
+                      <Alert severity="success">
+                        <AlertTitle>Success</AlertTitle>
+                        trade request created
+                      </Alert>
+                      );
+                      // alert('trade request created \nadd ' + addClass.class + ': ' + addClass.section + ' and drop ' + dropClass.class + ': ' + dropClass.section);
+                    }
+                    else { 
+                      setAlert(
+                        <Alert severity="error">
+                          <AlertTitle>Failure</AlertTitle>
+                          trade already exists
+                        </Alert>
+                        );
+                    }
                   }
                 }}
               >
                 Create Trade
               </Button>
+              <div className="Alert">{alert}</div>
             </Box>
         </Box>
       <Footer/>

fb89c28a85f40ea156762d1acca740c134ab5f7e
ryanjw0903 <60299798+ryanjw0903@users.noreply.github.com>
2022-04-04 09:42:43 -0500

    unused fix

--- src/comp/marketplace/Marketplace.js
+++ src/comp/marketplace/Marketplace.js
@@ -9,7 +9,7 @@ import { Button } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import Navbar from '../global/navbar/Navbar';
 import Footer from "../global/Footer";
-import { updateTradeMatch, createTrade, getReviews, addReviews } from "../global/dbFunctions/CrudFunctions"
+import { updateTradeMatch, createTrade, getReviews} from "../global/dbFunctions/CrudFunctions"
 import Chip from '@mui/material/Chip';
 
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
@@ -217,6 +217,8 @@ function Marketplace() {
   //   setRowsPerPage(parseInt(event.target.value, 10));
   //   setPage(0);
   // };
+
+
 
   function getBio(id) {
     (async () => {

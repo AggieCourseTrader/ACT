25c1d70c6577dbb6142a3ce3691f2daff3bc9c4c
Sean Wangnick <swangnick@gmail.com>
2022-04-08 11:28:01 -0500

    changes

--- src/comp/messages/CloseConversation.js
+++ src/comp/messages/CloseConversation.js
@@ -6,6 +6,13 @@ import Typography from '@mui/material/Typography';
 import {arrayRemove, updateDoc, doc} from "firebase/firestore";
 import {addReviews, db, getTradeId} from "../global/dbFunctions/CrudFunctions";
 import {Avatar} from "@chatscope/chat-ui-kit-react"
+import {TextField} from "@mui/material";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
 
 const style = {
   position: 'absolute',
@@ -21,19 +28,24 @@ const style = {
 
 const writeReview = async (text, user, activeConversationObj, tradeSuccess, positiveExperience) => {
   // We need to get the trade id from firestore
-  if(!(tradeId in activeConversationObj)) {
+  if(!("tradeId" in activeConversationObj)) {
     return;
   }
 
-  const f = await addReviews(user.uid, text, activeConversationObj.trade_id, tradeSucess, positiveExperience);
+  const f = await addReviews(user.uid, text, activeConversationObj.trade_id, tradeSuccess, positiveExperience);
 
   return f;
 }
 export default function CloseConversation({user, setActiveConversation, setActiveConversationObj, activeConversation, activeConversationObj, open, handleClose}) {
 
+  const [next, setNext] = React.useState(false);
+
+  const [reviewText, setReviewText] = React.useState("");
+  const [experience, setExperience] = React.useState("");
+  const [success, setSuccess] = React.useState("");
+
   return (
       <>
-      {(val == true) ?  true : false}
         <Modal
             open={open}
             onClose={handleClose}
@@ -41,49 +53,110 @@ export default function CloseConversation({user, setActiveConversation, setActiv
             aria-describedby="modal-modal-description"
         >
           <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Close Conversation
-            </Typography>
-            <Avatar src={activeConversationObj.photoURL}/>
-            <Typography id="modal-modal-description" sx={{mt: 2}}>
-              Are you sure you want to finalize this trade and close off the conversation
-              with {activeConversationObj.fname}?
-            </Typography>
-            <Button variant="contained" color="primary" onClick={handleClose}>
-              Cancel
-            </Button>
-            <Button variant="contained" color="secondary" onClick={async (e) => {
+            {(next === false) ?
+                <div>
+                  <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Close Conversation
+                  </Typography>
+                  <Typography id="modal-modal-description" sx={{mt: 2}}>
+                    Are you sure you want to finalize this trade and close off the conversation
+                    with {activeConversationObj.fname}?
+                  </Typography>
+                  <br></br>
+                  <Button variant="contained" color="primary" onClick={handleClose}>
+                    Cancel
+                  </Button>
+                  <Button variant="contained" color="secondary" onClick={() => {
+                    setNext(true)
+                  }}>
+                    Next
+                  </Button>
+                </div>
+              :
+                <div>
+                  <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Leave Feedback
+                  </Typography>
+                  <Avatar src={activeConversationObj.photoURL}/>
+                  {activeConversationObj.fname + " " + activeConversationObj.lname}
+                  <br></br>
+                  <TextField
+                      onChange={(e) => {
+                        setReviewText(e.target.value)
+                      }}
+                      style={{textAlign: 'left'}}
+                      hintText="Message Field"
+                      floatingLabelText="MultiLine and FloatingLabel"
+                      multiline
+                      rows={3}
+                  />
+                  <br></br>
+                  <FormControl>
+                    <FormLabel id="question-experience">Did you have a positive experience?</FormLabel>
+                      <RadioGroup
+                          row
+                          onChange={(v) => {
+                            setExperience(v)
+                          }}
+                      >
+                        <FormControlLabel value="yes" control={<Radio/>} label="Yes"/>
+                        <FormControlLabel value="no" control={<Radio/>} label="No"/>
+                      </RadioGroup>
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel id="question-experience">Was the trade successful?</FormLabel>
+                    <RadioGroup
+                        row
+                        onChange={(v) => {
+                          setSuccess(v)
+                        }}
+                    >
+                      <FormControlLabel value="yes" control={<Radio/>} label="Yes"/>
+                      <FormControlLabel value="no" control={<Radio/>} label="No"/>
+                    </RadioGroup>
+                  </FormControl>
+                  <br></br>
+                  <Button variant="contained" color="primary" onClick={() => {
+                    setNext(false)
+                  }}>
+                    Back
+                  </Button>
+                  <Button variant="contained" color="secondary" onClick={async (e) => {
 
-              await updateDoc(doc(db, "messageStatus", user.uid), {
-                "activeConversations": arrayRemove(activeConversationObj)
-                // [g] : increment(1)
-              });
+                    writeReview(reviewText, user, activeConversationObj, success, experience);
 
-              const data = {
-                "id": user.uid,
-                "fname": user.displayName.split(" ")[0],
-                "lname": user.displayName.split(" ")[1],
-                "photoURL": user.photoURL,
-                "addClass": activeConversationObj.dropClass,
-                "addClassSection": activeConversationObj.dropClassSection,
-                "dropClass": activeConversationObj.addClass,
-                "dropClassSection": activeConversationObj.addClassSection,
-              };
+                    await updateDoc(doc(db, "messageStatus", user.uid), {
+                      "activeConversations": arrayRemove(activeConversationObj)
+                      // [g] : increment(1)
+                    });
 
-              await updateDoc(doc(db, "messageStatus", activeConversation), {
-                "activeConversations": arrayRemove(data)
-              });
+                    const data = {
+                      "id": user.uid,
+                      "fname": user.displayName.split(" ")[0],
+                      "lname": user.displayName.split(" ")[1],
+                      "photoURL": user.photoURL,
+                      "addClass": activeConversationObj.dropClass,
+                      "addClassSection": activeConversationObj.dropClassSection,
+                      "dropClass": activeConversationObj.addClass,
+                      "dropClassSection": activeConversationObj.addClassSection,
+                    };
 
-              handleClose();
-              setActiveConversation("");
-              setActiveConversationObj({
-                fname : "",
-                lname : "",
-                photoURL : "",
-            });
-            }}>
-              Close
-            </Button>
+                    await updateDoc(doc(db, "messageStatus", activeConversation), {
+                      "activeConversations": arrayRemove(data)
+                    });
+
+                    handleClose();
+                    setActiveConversation("");
+                    setActiveConversationObj({
+                      fname : "",
+                      lname : "",
+                      photoURL : "",
+                    });
+                  }}>
+                    Submit
+                  </Button>
+                </div>
+            }
           </Box>
         </Modal>
       </>

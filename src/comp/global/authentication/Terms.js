c87eb3085adb8882dacf098f3276481bd808df97
ryanjw0903 <60299798+ryanjw0903@users.noreply.github.com>
2022-04-18 10:03:21 -0500

    TOS complete

--- src/comp/global/authentication/Terms.js
+++ src/comp/global/authentication/Terms.js
@@ -8,6 +8,8 @@ import { auth, onAuthStateChanged} from "../../../firebase-config";
 import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
 import TermsContext from './TermsContext';
+import Privacy from '../../../assets/PrivacyPolicy.pdf'
+import ToS from '../../../assets/ToS.pdf'
 
 const useStyles = makeStyles({
     wrapper: {
@@ -16,7 +18,7 @@ const useStyles = makeStyles({
       justifyContent: 'center',
       flexWrap: 'wrap',
       textAlign: "center",
-      marginTop: "2%"
+      marginTop: "1%",
     },
     container: {
       display: 'flex',
@@ -26,6 +28,18 @@ const useStyles = makeStyles({
       marginRight: "10%",
       marginLeft: "10%",
       flexWrap: 'wrap',
+    },
+    containerList: {
+        display: 'flex',
+        justifyContent: 'start',
+        textAlign: 'left',
+        width: '100%',
+        marginRight: "10%",
+        marginLeft: "10%",
+        flexWrap: 'wrap',
+      },
+    list: {
+        listStyleType: 'lower-alpha'
     }
   });
 
@@ -74,13 +88,62 @@ function TermsPage() {
         <React.Fragment>
             <div className={classes.wrapper}>
                 <div className={classes.container}>
-                    <Typography id="modal-modal-title" variant="h2">Terms of Service</Typography>
+                    <Typography id="modal-modal-title" variant="h3">General Guidelines</Typography>
                 </div>
             </div>
             <div className={classes.wrapper}>
-                <div className={classes.container}>
-                    <Typography id="modal-modal-title" variant="body1">
-                        i AGree to not be a dick
+                <div className={classes.containerList}>
+                    <Typography id="modal-modal-title" variant="h6">
+                     Course Trading 
+                    </Typography>
+                </div>
+            </div><div className={classes.wrapper}>
+                <div className={classes.containerList}>
+                    <ol className={classes.list}>
+                       <Typography id="modal-modal-title" variant="body1">
+                            <li>Aggie Course Trader does not support actual course trading within the TAMU system.</li>
+                            <li>This application only facilitates user interactions for them to do it on their own.</li>
+                            <li>We are not responsible for any failing trade or for another party backing out or not going through with a agreed upon trade in our app.</li>
+                            <li>Warning, some classes have waitlists or other advisor oversight that can lead to a failure of trade. We recommend you check the class on howdy before dropping or adding any class.</li>
+                        </Typography>
+                    </ol>
+                </div>
+            </div>
+            <div className={classes.wrapper}>
+                <div className={classes.containerList}>
+                    <Typography id="modal-modal-title" variant="h6">
+                      Privacy
+                    </Typography>
+                </div>
+            </div>
+            <div className={classes.wrapper}>
+                <div className={classes.containerList}>
+                    <ol className={classes.list}>
+                       <Typography id="modal-modal-title" variant="body1">
+                            <li>Select user TAMU information will be stored. The only info stored is first and last name, plus your tamu email address.</li>
+                            <li>All reviews will use your first and last name and will not be anonymous, and there is no option to be anonymous.</li>
+                        </Typography>
+                    </ol>
+                </div>
+            </div>
+            <div className={classes.wrapper}>
+                <div className={classes.containerList}>
+                    <Typography id="modal-modal-title" variant="h6">
+                     Honor Code
+                    </Typography>
+                </div>
+            </div>
+            <div className={classes.wrapper}>
+                <div className={classes.containerList}>
+                       <Typography id="modal-modal-title" variant="body1">
+                            An Aggie does not lie, cheat or steal or tolerate those who do.
+                        </Typography>
+                </div>
+            </div>
+            <div className={classes.wrapper}>
+                <div className={classes.containerList}>
+                    <Typography id="modal-modal-title" variant="caption">
+                        View our full <a href={Privacy} target="_blank">Privacy Policy</a> and <a href={ToS} target="_blank">Terms of Service</a>
                     </Typography>
                 </div>
             </div>
@@ -89,7 +152,7 @@ function TermsPage() {
                 <Checkbox
                     onChange={handleCheck}
                 />
-                <Typography id="modal-modal-title" variant="body1">By checking this box i agree to the terms of service above</Typography>
+                <Typography id="modal-modal-title" variant="body1">By checking this box I agree to the above</Typography>
                 </div>
             </div>
             <div className={classes.wrapper}>

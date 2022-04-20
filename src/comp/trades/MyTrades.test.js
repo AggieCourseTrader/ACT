514ae0228fa856a2ad273bed763111a67a6747d9
jeroda7105@gmail.com <54687259+jeroda7105@users.noreply.github.com>
2022-04-20 10:02:22 -0500

    Added tests for marketplace and mytrades

--- src/comp/trades/MyTrades.test.js
+++ src/comp/trades/MyTrades.test.js
@@ -32,17 +32,13 @@ return {
 });
 
 
-
-
 test('renders MyTrades component', async () => {
 
     render(<BrowserRouter><MyTrades /></BrowserRouter>);
 
-    
+    // Checks for text rendering
     expect(await screen.findByText(/My Listings/)).toBeInTheDocument();
-    
     expect(await screen.findByText(/My Matches/)).toBeInTheDocument();
-    
 
 });
 
